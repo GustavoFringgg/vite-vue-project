0aee0be013d541b905f214c09419f32abc507865
Theodore <adamtsai0408@gmail.com>
2024-09-02 20:40:29 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<!-- 主要有三個結構1.script 2. template 3.style -->
+<!-- script = vue -->
+<!-- template = html片段 -->
+<!-- style = css -->
+<script setup>
+//setp =>不用****return****
+import { ref } from 'vue'
+const num = ref(1)
+console.log('num:', num.value)
+
+// setInterval(() => {
+//   num.value++
+// }, 500)
+function clickMe() {
+  num.value++
+}
+</script>
+
+<template>
+  <div>
+    {{ num }}
+    <button type="button" v-on:click="clickMe">按我</button>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/main.js
@@ -0,0 +1,6 @@
+// import './assets/main.css'
+
+import { createApp } from 'vue' //匯入vue方法並初始化
+import App from './App.vue'
+
+createApp(App).mount('#app')
