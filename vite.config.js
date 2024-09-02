7181052ec80786b96b668859cc7c53862b00fbd4
Theodore <adamtsai0408@gmail.com>
2024-09-02 20:48:33 +0800

    correct url

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/vite-vue-project/',
   plugins: [vue()],
   resolve: {
     alias: {
